87d4a70c343b205aae196ef572936d4d2ecf2c1b
typhoon <typhoon@iMac-Dmitrij.local>
2022-05-29 22:05:09 +1000

    updated

--- main.py
+++ main.py
@@ -20,15 +20,9 @@ from api import author_router, def_router, term_router, type_router, role_router
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-    "http://localhost:8080",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
